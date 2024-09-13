c3e8d7304c176c52870b75d93e7725014eb100c1
lautiibarreras <l.barreras@alumno.etec.um.edu.ar>
2024-09-13 09:27:14 -0300

    Token y Auth

--- src/rutas/appointmentRutas.js
+++ src/rutas/appointmentRutas.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { asignarCita, obtenerCitasPorUsuario } from '../controladores/appointmentControlador.js';
+import { verificarToken } from '../middlewares/authMiddleware.js'; // Importar el middleware
 
 const router = express.Router();
 
-router.post('/asignar', asignarCita);
-router.get('/usuario/:user_id', obtenerCitasPorUsuario);
+// Proteger la ruta para asignar una cita con el middleware verificarToken
+router.post('/asignar', verificarToken, asignarCita);
+
+// Proteger la ruta para obtener las citas de un usuario con el middleware verificarToken
+router.get('/usuario/:user_id', verificarToken, obtenerCitasPorUsuario);
 
 export default router;
